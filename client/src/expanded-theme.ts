21e23085788999a4cd01642ed320d1fabce0b26b
Maxwell Calkin <maxwellcalkin@gmail.com>
2023-09-09 18:51:13 -0400

    fixing expanded-theme

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,6 +1,10 @@
 //eslint-disable-next-line
 import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
+const hello: PaletteColor[] = [];
+const goodbye: Palette[] = [];
+console.log(hello, goodbye);
+
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
     [key: number]: string;
